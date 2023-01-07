3dbb9a6ded3c2c40e51816856e240d8e6986920b
Orkun Ozoglu <50372354+ozogxyz@users.noreply.github.com>
2023-01-07 13:42:21 +0300

    exp-paper test distinguish

--- tests/test_paper.py
+++ tests/test_paper.py
@@ -1,19 +1,19 @@
-from src.models.components.cnn_lstm_net import CNNLSTMNet
+from src.models.components.paper import Paper
 import torch
 import pytest
 
 
 def test_cnn_lstm_net_init():
-    cnn_lstm_net = CNNLSTMNet(conv_out=32, lstm_hidden=32)
-    assert cnn_lstm_net is not None
-    assert isinstance(cnn_lstm_net, CNNLSTMNet)
+    paper_net = Paper(conv_out=32, lstm_hidden=32)
+    assert paper_net is not None
+    assert isinstance(paper_net, Paper)
 
 
 @pytest.mark.parametrize("batch_size", [32])
 def test_cnn_lstm_net_forward(batch_size: int):
-    cnn_lstm_net = CNNLSTMNet(conv_out=32, lstm_hidden=32)
+    paper_net = Paper(conv_out=32, lstm_hidden=32)
     x = torch.randn(batch_size, 14, 30)
-    y = cnn_lstm_net(x)
+    y = paper_net(x)
     assert y is not None
     assert isinstance(y, torch.Tensor)
     assert y.shape == torch.Size([batch_size])
