87e7777574b4a60c6d4746cd9e9b2d7e56fe6191
ozogxyz <molts.rails_0d@icloud.com>
2023-01-09 19:22:52 +0000

    add kernel stride to args

--- src/models/components/experiment.py
+++ src/models/components/experiment.py
@@ -18,8 +18,6 @@ class ConvBlock(nn.Module):
             kernel_size,
             stride,
             padding,
-            # padding_mode="reflect",
-            # dilation=2,
         )
         self.bn = nn.BatchNorm1d(out_channels)
         self.relu = nn.ReLU(inplace=True)
@@ -34,42 +32,38 @@ class ConvBlock(nn.Module):
 
 
 class ExpNet(nn.Module):
-    def __init__(self, conv_out: int, lstm_hidden: int):
+    def __init__(
+        self,
+        conv_out: int,
+        lstm_hidden: int,
+        kernel_size: int,
+        stride: int,
+        padding: int,
+    ):
         super(ExpNet, self).__init__()
 
         self.feature_extractor = nn.Sequential(
-            ConvBlock(14, conv_out, kernel_size=5, stride=1, padding=1),
-            ConvBlock(conv_out, 2 * conv_out, kernel_size=3, stride=2, padding=1),
+            ConvBlock(14, conv_out, kernel_size, stride, padding),
+            ConvBlock(conv_out, 2 * conv_out, kernel_size-2, stride, padding),
         )
 
         self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
-        # self.lstm = nn.LSTM(320, lstm_hidden, 2, batch_first=True, dropout=0.2)
-        self.recurrent = nn.LSTM(448, lstm_hidden, 2, batch_first=True, dropout=0.2)
+        self.recurrent = nn.LSTM(256, lstm_hidden, 2, batch_first=True, dropout=0.2)
         self.recurrent_activation = nn.Tanh()
 
         self.dense = nn.Sequential(
             nn.Linear(32, 16),
             nn.ReLU(inplace=True),
-            # nn.Dropout(p=0.5),
             nn.Linear(16, 8),
             nn.ReLU(inplace=True),
-            # nn.Dropout(p=0.5),
             nn.Linear(8, 1),
         )
 
     def forward(self, x: torch.Tensor):
         x = self.feature_extractor(x)
-        # print(x.shape)
         x = self.pool(x)
-        # print(x.shape)
-        # x = self.lstm(x)[0]
-        # x = x.permute(0, 2, 1)
         x = x.view(x.size(0), -1)
-        # print(x.shape)
         x, _ = self.recurrent(x)
-        # print(x.shape)
         x = self.recurrent_activation(x)
-        # print(x.shape)
         x = self.dense(x)
-        print(f'##$^#$^#$%#$%^#$%^#$%^#$%^ Prediction: {x.flatten()} #$%^#$%^#$%^#$%^#$%^#$%^#$%^')
         return x.reshape(-1)
