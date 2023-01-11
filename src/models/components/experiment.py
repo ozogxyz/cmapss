f043485e8dcc1167f03d3d486694f995fd8acf6c
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2023-01-11 13:05:02 +0000

    new sweep with kernel and stride parametrized

--- src/models/components/experiment.py
+++ src/models/components/experiment.py
@@ -3,14 +3,21 @@ import torch.nn as nn
 
 
 class ExpNet(nn.Module):
-    def __init__(self, conv_out: int, lstm_hidden: int):
+    """Experimental network for time series foreacasting."""
+    def __init__(self, conv_out: int, kernel_size: int, stride:int, lstm_hidden: int):
         super(ExpNet, self).__init__()
 
-        self.conv1 = nn.Conv1d(14, conv_out, kernel_size=5, stride=1, padding=1)
-        self.conv2 = nn.Conv1d(conv_out, conv_out*2, kernel_size=3, stride=2, padding=1)
+        self.conv1 = nn.Conv1d(14, conv_out, kernel_size, stride, padding=1)
+        self.conv2 = nn.Conv1d(conv_out, conv_out*2, kernel_size-2, stride, padding=1)
 
         self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
-        self.lstm = nn.LSTM(384, lstm_hidden, 2, batch_first=True, dropout=0.2)
+
+        conv1_out_dim: int = (32 - kernel_size) // stride + 1
+        conv2_out_dim: int = (conv1_out_dim + 2 - (kernel_size - 2)) // (stride) + 1
+        pool_out_dim: int = (conv2_out_dim - 2) // 2 + 1
+
+
+        self.lstm = nn.LSTM(conv_out * 2 * pool_out_dim, lstm_hidden, 2, batch_first=True, dropout=0.2)
 
         self.fc1 = nn.Linear(lstm_hidden, 16)
         self.fc2 = nn.Linear(16, 8)
--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -3,51 +3,16 @@ import torch
 import pytest
 
 
-# @pytest.mark.parametrize("batch_size", [32, 64])
-# @pytest.mark.parametrize("window_size", [30, 40])
-# @pytest.mark.parametrize("in_channels", [14, 21])
-# @pytest.mark.parametrize("out_channels", [14, 21])
-# @pytest.mark.parametrize("kernel_size", [3, 5])
-# @pytest.mark.parametrize("stride", [1, 2])
-# @pytest.mark.parametrize("padding", [1, 2])
-# def test_conv_block(
-#     batch_size: int, window_size: int, in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int
-# ):
-#     input = torch.randn(batch_size, in_channels, window_size)
-#     model = ConvBlock(in_channels, out_channels, kernel_size, stride, padding)
-#     output = model(input)
+@pytest.fixture
+def batch_size():
+    return 32
 
-#     assert output.shape == model._output_shape_hook
-#     assert output.dtype == torch.float32
-
-# @pytest.mark.parametrize("batch_size", [32, 64])
-# @pytest.mark.parametrize("window_size", [30])
-# @pytest.mark.parametrize("in_channels", [14, 21])
-# @pytest.mark.parametrize("out_channels", [14, 21])
-# @pytest.mark.parametrize("kernel_size", [3, 5])
-# @pytest.mark.parametrize("stride", [1, 2])
-# @pytest.mark.parametrize("padding", [1, 2])
-# @pytest.mark.parametrize("num_layers", [1, 2])
-# def test_convolution_lstm(
-#     batch_size: int, window_size: int, in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int, num_layers: int
-# ):
-#     input = torch.randn(batch_size, in_channels, window_size)
-#     model = ConvolutionLSTM(in_channels, out_channels, kernel_size, stride, padding, num_layers)
-#     output = model(input)
-
-#     assert output.shape == model._output_shape_hook
-#     assert output.dtype == torch.float32
-
-
-
-@pytest.mark.parametrize("batch_size", [32])
-def test_conv_forward(batch_size: int):
-    experiment_net = ExpNet(conv_out=32, lstm_hidden=32)
-    x = torch.randn(batch_size, 14, 30)
-    y = experiment_net(x)
-    assert y is not None
-    assert isinstance(y, torch.Tensor)
-    assert y.shape == torch.Size([batch_size])
-
-    assert output.shape == model._output
-    assert output.dtype == torch.float32
+@pytest.mark.parametrize("conv_out", [32, 64])
+@pytest.mark.parametrize("kernel_size", [5])
+@pytest.mark.parametrize("stride", [1, 2, 3])
+@pytest.mark.parametrize("lstm_hidden", [32, 50])
+def test_conv_forward(batch_size: int, conv_out: int, kernel_size: int, stride: int, lstm_hidden: int) -> None:
+    experiment_net = ExpNet(conv_out, kernel_size, stride, lstm_hidden)
+    input = torch.randn(batch_size, 14, 30)
+    output = experiment_net(input)
+    assert output.shape == torch.Size([batch_size])
