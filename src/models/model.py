0db0c8fea8151755eb3de5abc68ee92d1b06e241
Orkun Ozoglu <50372354+ozogxyz@users.noreply.github.com>
2023-01-12 15:30:49 +0300

    maxpool stride 1

--- src/models/model.py
+++ src/models/model.py
@@ -11,15 +11,13 @@ class CNNLSTM(nn.Module):
         self.conv1 = nn.Conv1d(14, conv_out, kernel_size, stride, padding=1)
         self.conv2 = nn.Conv1d(conv_out, conv_out * 2, kernel_size - 2, stride, padding=1)
 
-        self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.pool = nn.MaxPool1d(kernel_size=2, stride=1)
 
         conv1_out_dim: int = (32 - kernel_size) // stride + 1
         conv2_out_dim: int = (conv1_out_dim + 2 - (kernel_size - 2)) // (stride) + 1
-        pool_out_dim: int = (conv2_out_dim - 2) // 2 + 1
+        pool_out_dim: int = (conv2_out_dim - 2) // 1 + 1
 
-        self.lstm = nn.LSTM(
-            conv_out * 2 * pool_out_dim, lstm_hidden, 2, batch_first=True, dropout=0.2
-        )
+        self.lstm = nn.LSTM(conv_out * 2 * pool_out_dim, lstm_hidden, 2, batch_first=True, dropout=0.2)
 
         self.fc1 = nn.Linear(lstm_hidden, 16)
         self.fc2 = nn.Linear(16, 8)
@@ -28,19 +26,20 @@ class CNNLSTM(nn.Module):
         self.tanh = nn.Tanh()
         self.relu = nn.ReLU()
 
-    def forward(self, x: torch.Tensor):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.pool(x)
+    def forward(self, input: torch.Tensor):
+        input = self.conv1(input)
+        input = self.conv2(input)
+        input = self.pool(input)
 
-        x = x.view(x.size(0), -1)
-        x, _ = self.lstm(x)
-        x = self.tanh(x)
+        input = input.view(input.size(0), -1)
+        input, _ = self.lstm(input)
+        input = self.tanh(input)
 
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-        x = self.relu(x)
-        x = self.fc3(x)
+        input = self.fc1(input)
+        input = self.relu(input)
+        input = self.fc2(input)
+        input = self.relu(input)
+        output = self.fc3(input)
 
-        return x.reshape(-1)
+        output = output.reshape(-1)
+        return output
