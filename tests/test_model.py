8a9e6dafd6cfee654687788e1145c73cfdc71fa8
Orkun Ozoglu <50372354+ozogxyz@users.noreply.github.com>
2023-01-12 21:57:57 +0300

    cleanup tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -9,14 +9,12 @@ def batch_size():
     return 32
 
 
-@pytest.mark.parametrize("conv_out", [32, 64])
-@pytest.mark.parametrize("kernel_size", [5])
-@pytest.mark.parametrize("stride", [1, 2, 3])
+@pytest.mark.parametrize("conv_out", [32])
 @pytest.mark.parametrize("lstm_hidden", [32, 50])
 def test_conv_forward(
-    batch_size: int, conv_out: int, kernel_size: int, stride: int, lstm_hidden: int
+    batch_size: int, conv_out: int, lstm_hidden: int
 ) -> None:
-    model = CNNLSTM(conv_out, kernel_size, stride, lstm_hidden)
+    model = CNNLSTM(conv_out, lstm_hidden)
     input = torch.randn(batch_size, 14, 30)
     output = model(input)
     assert output.shape == torch.Size([batch_size])
