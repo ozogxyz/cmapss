857ee1e556e03bd87e9d7c94bda68b591b27e6c5
Orkun Ozoglu <50372354+ozogxyz@users.noreply.github.com>
2023-01-12 22:07:28 +0300

    black

--- tests/test_model.py
+++ tests/test_model.py
@@ -12,9 +12,7 @@ def batch_size():
 @pytest.mark.parametrize("conv_out", [32])
 @pytest.mark.parametrize("conv_out", [32])
 @pytest.mark.parametrize("lstm_hidden", [32, 50])
-def test_conv_forward(
-    batch_size: int, conv_out: int, lstm_hidden: int
-) -> None:
+def test_conv_forward(batch_size: int, conv_out: int, lstm_hidden: int) -> None:
     model = CNNLSTM(conv_out, lstm_hidden)
     input = torch.randn(batch_size, 14, 30)
     output = model(input)
