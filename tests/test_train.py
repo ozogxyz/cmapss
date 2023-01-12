c7825709664c49f9be8d17753180abb72a8b1edf
Orkun Ozoglu <50372354+ozogxyz@users.noreply.github.com>
2023-01-12 15:31:47 +0300

    delete test_train_ddp_sim

--- tests/test_train.py
+++ tests/test_train.py
@@ -50,18 +50,6 @@ def test_train_epoch_double_val_loop(cfg_train):
 
 
 @pytest.mark.slow
-def test_train_ddp_sim(cfg_train):
-    """Simulate DDP (Distributed Data Parallel) on 2 CPU processes."""
-    HydraConfig().set_config(cfg_train)
-    with open_dict(cfg_train):
-        cfg_train.trainer.max_epochs = 2
-        cfg_train.trainer.accelerator = "cpu"
-        cfg_train.trainer.devices = 2
-        cfg_train.trainer.strategy = "ddp_spawn"
-    train(cfg_train)
-
-
-@pytest.mark.slow
 def test_train_resume(tmp_path, cfg_train):
     """Run 1 epoch, finish, and resume for another epoch."""
     with open_dict(cfg_train):
